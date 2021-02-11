56ccf723ff9f93562ae2017437713981393d8459
Chris86M <crismen19862017@gmail.com>
2021-02-11 19:15:05 +0000

    alphabetical

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+	}
+	int letters = "z";
+
+while
+(letters >= 'a');
+
+	{
+		putchar(letters);
+		letters--;
+	}
+	putchar('\n');
+	return (0);
+}
